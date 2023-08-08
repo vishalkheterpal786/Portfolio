f76b2b7a5fc5794206af8f2cd38b8060acb90cc0
vishalkheterpal <vishalkheterpal786@gmail.com>
2023-08-08 16:12:10 +0200

    config-changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vishalkheterpal786/Portfolio",
+  base: "/Portfolio",
   plugins: [react()],
+  esbuild: {
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
+  },
 })
